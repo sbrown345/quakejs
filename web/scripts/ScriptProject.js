0e7a739a8eb89be911cd20d47082222da74b253a
Simon Brown <simonbrown33@gmail.com>
2013-02-02 11:31:35 +0000

    build js output to new webproject

---
+++ web/scripts/ScriptProject.js
@@ -0,0 +1,10 @@
+﻿(function() {
+	////////////////////////////////////////////////////////////////////////////////
+	// ScriptProject.Program
+	var $ScriptProject_$Program = function() {
+	};
+	$ScriptProject_$Program.$main = function() {
+	};
+	Type.registerClass(null, 'ScriptProject.$Program', $ScriptProject_$Program, Object);
+	$ScriptProject_$Program.$main();
+})();
