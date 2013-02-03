1be40feb711b430209b0251ef2b7e9227953e243
Simon Brown <simonbrown33@gmail.com>
2013-02-03 22:25:50 +0000

    fps stats

--- ScriptProject/InnoveWare/Page.cs
+++ ScriptProject/InnoveWare/Page.cs
@@ -7,57 +7,61 @@ namespace InnoveWare
 {
     using System.Windows.Controls;
 
+    using Missing.Libs;
+
+    using jQueryApi;
+
     public class Page
-   {
-       private int _frames_count = 0;
-       private int _last_frame = 0;
-       private DateTime _lastTime;
-       public static BitmapImage bitmap = new BitmapImage();
-       //private Dictionary<Key, int> dictionaryKeys;
-       private double oldtime;
-       public static Page thePage;
-       public static int gwidth;
-       public static int gheight;
-       private int count_fps = 0;
-       private int nb_fps = 0;
+    {
+        public static BitmapImage bitmap = new BitmapImage();
+        //private Dictionary<Key, int> dictionaryKeys;
+        private double oldtime;
+        public static Page thePage;
+        public static int gwidth;
+        public static int gheight;
+        public static Stats stats;
 
-       // faking stuff from xaml
-       public Canvas parentCanvas { get; set; }
-       public Image image { get; set; }
+        // faking stuff from xaml
+        public Canvas parentCanvas { get; set; }
+        public Image image { get; set; }
 
-       public Page()
-       {
-           parentCanvas = new Canvas();
-           thePage = this;
-           image = new Image();
-       }
+        public Page()
+        {
+            parentCanvas = new Canvas();
+            thePage = this;
+            image = new Image();
+            stats = new Stats();
+            stats.SetMode(0);
+            jQuery.Select("body").Append(stats.domElement);
+        }
 
-       public void Page_Loaded()
-       {
-           // hardcode for now
-           gwidth = 800; //(int)Application.Current.Host.Content.ActualWidth; //if this is bigger it was crashing because it was larger than 1024 and some buffer didn't liek it
-           gheight = 600;// (int)Application.Current.Host.Content.ActualHeight;
-           
-           // Initialize Quake.
-           oldtime = quake.sys_linux.Sys_FloatTime();
-           quake.sys_linux.main(0, null);
+        public void Page_Loaded()
+        {
+            // hardcode for now
+            gwidth = 800; //(int)Application.Current.Host.Content.ActualWidth; //if this is bigger it was crashing because it was larger than 1024 and some buffer didn't liek it
+            gheight = 600;// (int)Application.Current.Host.Content.ActualHeight;
 
-           // Game loop.
-           Page_CompositionTarget_Rendering();
-       }
+            // Initialize Quake.
+            oldtime = quake.sys_linux.Sys_FloatTime();
+            quake.sys_linux.main(0, null);
 
-       void Page_CompositionTarget_Rendering()
-       {
-           // Count frame.
-           _frames_count++;
+            // Game loop.
+            Page_CompositionTarget_Rendering();
+        }
 
-           // Synchronize the Silverlight UI thread to Quake framerate.
-           double newtime = quake.sys_linux.Sys_FloatTime();
-           double time = newtime - oldtime;
-           quake.host.Host_Frame(time);
-           oldtime = newtime;
-           
-           Window.RequestAnimationFrame(Page_CompositionTarget_Rendering);
-       }
-   }
+        void Page_CompositionTarget_Rendering()
+        {
+            stats.Begin();
+
+            // Synchronize the Silverlight UI thread to Quake framerate.
+            double newtime = quake.sys_linux.Sys_FloatTime();
+            double time = newtime - oldtime;
+            quake.host.Host_Frame(time);
+            oldtime = newtime;
+
+            Window.RequestAnimationFrame(Page_CompositionTarget_Rendering);
+
+            stats.End();
+        }
+    }
 }
---
+++ ScriptProject/Missing/Libs/Stats.cs
@@ -0,0 +1,25 @@
+﻿namespace Missing.Libs
+{
+    using System.Html;
+    using System.Runtime.CompilerServices;
+
+    [Imported]
+    [ModuleName(null)]
+    [IgnoreNamespace]
+    public class Stats
+    {
+        public void SetMode(int mode)
+        {
+        }
+
+        public void Begin()
+        {
+        }
+
+        public void End()
+        {
+        }
+
+        public Element domElement;
+    }
+}
