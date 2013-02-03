0eda1c1d109f7e83af7caa81d0c38e0c742ac1ae
Simon Brown <simonbrown33@gmail.com>
2013-02-03 19:49:02 +0000

    random class (thanks - http://ninoporcino.blogspot.co.uk/2012/12/saltarelle-vs-jsil-raytracer-demo.html)

--- ScriptProject/Missing/System/Random.cs
+++ ScriptProject/Missing/System/Random.cs
@@ -1,10 +1,60 @@
 ﻿namespace System
 {
+    using System.Runtime.CompilerServices;
+
+    using Missing;
+
+    [Imported]
+    public class MersenneTwister
+    {
+        [InlineCode("new MersenneTwister({seed})")]
+        public MersenneTwister(long seed)
+        {
+        }
+
+        [InlineCode("{this}.genrand_real1()")]
+        public float genrand_real1()
+        {
+            return 0;
+        }
+    }
+
     public class Random
     {
+        MersenneTwister mt;
+
+        public Random()
+        {
+            long ticks = DateTime.Now.GetTicks();
+            mt = new MersenneTwister(ticks); //Environment.TickCount - actual MS Random
+        }
+
+        public Random(long seed)
+        {
+            mt = new MersenneTwister(seed);
+        }
+
+        public double NextDouble()
+        {
+            return mt.genrand_real1();
+        }
+
         public int Next()
         {
-            throw new NotImplementedException();
+            float real = this.mt.genrand_real1();
+            return FloorInt(real * int.MaxValue);
+        }
+
+        public int Next(float maxValue)
+        {
+            float real = this.mt.genrand_real1();
+            return FloorInt(real * maxValue);
+        }
+
+        [InlineCode("Math.floor({value})")]
+        public static int FloorInt(float value)
+        {
+            return 0;
         }
     }
 }
