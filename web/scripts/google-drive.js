9eb1666d35f1bd65122458ae26249e49a25ba490
Simon Brown <simonbrown33@gmail.com>
2013-03-07 21:34:20 +0000

    download file g drive

--- web/scripts/google-drive.js
+++ web/scripts/google-drive.js
@@ -155,55 +155,47 @@ function insertFileFromBase64(filename, parentId, base64Data, callback) {
 }
 
 function base64ArrayBuffer(arrayBuffer) {
-    var base64 = ''
-    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
-    var bytes = new Uint8Array(arrayBuffer)
-    var byteLength = bytes.byteLength
-    var byteRemainder = byteLength % 3
-    var mainLength = byteLength - byteRemainder
-
-    var a, b, c, d
-    var chunk
-
-    // Main loop deals with bytes in chunks of 3
+    var base64 = '';
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    var bytes = new Uint8Array(arrayBuffer);
+    var byteLength = bytes.byteLength;
+    var byteRemainder = byteLength % 3;
+    var mainLength = byteLength - byteRemainder;
+    var a, b, c, d;
+    var chunk; // Main loop deals with bytes in chunks of 3
     for (var i = 0; i < mainLength; i = i + 3) {
         // Combine the three bytes into a single integer
-        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
-
-        // Use bitmasks to extract 6-bit segments from the triplet
-        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
-        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
-        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
-        d = chunk & 63               // 63       = 2^6 - 1
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]; // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
+        d = chunk & 63; // 63       = 2^6 - 1
 
         // Convert the raw binary segments to the appropriate ASCII encoding
-        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
     }
 
     // Deal with the remaining bytes and padding
     if (byteRemainder == 1) {
-        chunk = bytes[mainLength]
-
-        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+        chunk = bytes[mainLength];
+        a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
 
         // Set the 4 least significant bits to zero
-        b = (chunk & 3) << 4 // 3   = 2^2 - 1
+        b = (chunk & 3) << 4; // 3   = 2^2 - 1
 
-        base64 += encodings[a] + encodings[b] + '=='
+        base64 += encodings[a] + encodings[b] + '==';
     } else if (byteRemainder == 2) {
-        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
-
-        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
-        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+        a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
 
         // Set the 2 least significant bits to zero
-        c = (chunk & 15) << 2 // 15    = 2^4 - 1
+        c = (chunk & 15) << 2; // 15    = 2^4 - 1
 
-        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+        base64 += encodings[a] + encodings[b] + encodings[c] + '=';
     }
 
-    return base64
+    return base64;
 }
 
 /**
@@ -322,17 +314,33 @@ function initStuff() {
         console.log("retrieveAllFiles", arguments);
 
         for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            if (file && file.title == "quake_js") {
-                $("body").append("<div><img src=\"" + file.iconLink + "\">" +
-                    "<a href=\"" + "#" + "\">" + file.title + "</a></div>");
+            var quakeFolder = files[i];
+            if (quakeFolder && quakeFolder.title == "quake_js") {
+                //var fileToUpload = new Uint8Array(10);
+                //fileToUpload[0] = 72;
+                //fileToUpload[1] = 73;
+                //insertFileFromBase64("test.txt", quakeFolder.id, base64ArrayBuffer(fileToUpload.buffer), function () {
+                //    console.log("created file");
+                //});
 
-                var fileToUpload = new Uint8Array(10);
-                fileToUpload[0] = 72;
-                fileToUpload[1] = 73;
-                insertFileFromBase64("test.txt", file.id, base64ArrayBuffer(fileToUpload.buffer), function () {
-                    console.log("created file");
+                retrieveAllFiles("'" + quakeFolder.id + "' in parents and trashed = false", function (quakeFiles) {
+                    console.log("retrieveAllFiles quake folder", arguments);
+                    for (var j = 0; j < quakeFiles.length; j++) {
+                        var quakeFile = quakeFiles[j];
+                        if (quakeFile) {
+                            var $link = $("<div><img src=\"" + quakeFile.iconLink + "\">" +
+                                "<a href=\"" + "#" + "\">" + quakeFile.title + "</a></div>");
+                            $link.find("a").click(function (e) {
+                                e.preventDefault();
+                                downloadFile(quakeFile, function () {
+                                    console.log("downloadFile", arguments);
+                                });
+                            });
+                            $("body").append($link);
+                        }
+                    }
                 });
+
                 return;
             }
         }
